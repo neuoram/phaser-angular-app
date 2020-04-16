07d5e8c5dc36e1788ef3933b12feff4fc09369e5
neuoram <marfaoui@github.com>
2020-04-16 22:54:41 +0200

    init

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+import { MainScene } from './main-scene';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      backgroundColor: "#18216D",
+      scene: [ MainScene ],
+      parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 100 }
+        }
+      }
+    };
+  }
+  ngOnInit() {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
